2351ab4c9845b5408a13aeb6f8b27c101fdb1e69
ebisLab <ebaiden@techie.com>
2020-11-30 13:44:30 -0500

    image pointer

--- src/components/dashboard/messageSection/messages.js
+++ src/components/dashboard/messageSection/messages.js
@@ -15,7 +15,8 @@ import Toolbar from '@material-ui/core/Toolbar';
 
 
 import AppBar from '@material-ui/core/AppBar';
-import useStyles from '../dash.styles'
+import useStyles from '../dash.styles';
+import {chatbubbles, imgbubble, chatContainer, avatar} from '../chatstyles'
 
 
 
@@ -26,36 +27,36 @@ function Messages({handleDrawerToggle, theme, mobileOpen, drawer, clickUserConta
     const classes = useStyles();
 
 
-    const chatbubbles=`
-        max-width: 330px;
-        background: white ;
-        border-radius:  20px 20px 0px;
-        margin:10px;
-        padding:10px;
-        box-shadow:  1px 2px 6px 6px rgb(0 0 0 / 0%), -1px 1px 5px 0px rgb(0 0 0 / 8%), 0px 1px 10px 0px rgb(255 21 248 / 6%) 
-    `
-    const imgbubble=`
-        max-width: 330px ;
-        background: white ;
-        border-radius:  20px 20px 0px 20px ;
-        margin:20px;
-        padding:0;
-        box-shadow: 1px 2px 6px 6px rgb(0 0 0 / 0%), -1px 1px 5px 0px rgb(0 0 0 / 8%), 0px 1px 10px 0px rgb(255 21 248 / 6%)
-    `
-    const avatar=`
-        width:50px;
-        height:50px;
-        border-radius:50%;
-        margin-top:35px;
-        color: white;
-        font-size: 2.5em;
-    `
-    const chatContainer=`
-        display:inline-flex
-    `    
+    // const chatbubbles=`
+    //     max-width: 330px;
+    //     background: white ;
+    //     border-radius:  20px 20px 0px;
+    //     margin:10px;
+    //     padding:10px;
+    //     box-shadow:  1px 2px 6px 6px rgb(0 0 0 / 0%), -1px 1px 5px 0px rgb(0 0 0 / 8%), 0px 1px 10px 0px rgb(255 21 248 / 6%) 
+    // `
+    // const imgbubble=`
+    //     max-width: 330px ;
+    //     background: white ;
+    //     border-radius:  20px 20px 0px 20px ;
+    //     margin:20px;
+    //     padding:0;
+    //     box-shadow: 1px 2px 6px 6px rgb(0 0 0 / 0%), -1px 1px 5px 0px rgb(0 0 0 / 8%), 0px 1px 10px 0px rgb(255 21 248 / 6%)
+    // `
+    // const avatar=`
+    //     width:50px;
+    //     height:50px;
+    //     border-radius:50%;
+    //     margin-top:35px;
+    //     color: white;
+    //     font-size: 2.5em;
+    // `
+    // const chatContainer=`
+    //     display:inline-flex
+    // `    
 
-    const shadow = `
-    box-shadow: 1px 2px 6px 6px rgb(0 0 0 / 0%), -1px 1px 5px 0px rgb(0 0 0 / 8%), 0px 1px 10px 0px rgb(255 21 248 / 6%)`
+    // const shadow = `
+    // box-shadow: 1px 2px 6px 6px rgb(0 0 0 / 0%), -1px 1px 5px 0px rgb(0 0 0 / 8%), 0px 1px 10px 0px rgb(255 21 248 / 6%)`
 
 
     function send(){
@@ -79,7 +80,7 @@ imgdiv.innerHTML = "This is a paragraph."
           <img src="${file}" class="img" style="border-radius:inherit; width: 300px; max-height: 100%!important;"/> </div></div>`:""}`
 
         chatbox.innerHTML += `<div class="nthType" data-myname style="`+chatContainer+`">
-        <div class="avi" data-aviavi style="`+avatar+`">${localStorage.getItem('myname')[0].toUpperCase()}</div>
+        <div class="avi" data-aviavi style="`+avatar("myname")+`">${localStorage.getItem('myname')[0].toUpperCase()}</div>
         <div class="bubble" style="`+chatbubbles+`">
         ${message}</div></div>`
         document.querySelector("#text").value="";
@@ -91,12 +92,11 @@ imgdiv.innerHTML = "This is a paragraph."
     useEffect(()=>{
         let chatbox = document.querySelector(".contactprofile")
         socket.on('private', function(data){
-            chatbox.innerHTML += `${file?"yes":"no"}`
             // chatbox.innerHTML+=`${file ?`<div class="nthType" data-myname style="`+chatContainer+`">
             // <div class="bubble" style="`+imgbubble+`">
             //   <img src="https://picsum.photos/200" class="img" style="border-radius:inherit; width: 300px; max-height: 100%!important;"/> </div></div>`:""}`
     
-            chatbox.innerHTML += `<div class="nthType" data-theirname style="`+chatContainer+`"><div class="avi" style="`+avatar+`">${localStorage.getItem('receiver-username')[0].toUpperCase()}</div><div class="bubble" style="`+chatbubbles+`">${data.message}</div></div>`
+            chatbox.innerHTML += `<div class="nthType" data-theirname style="`+chatContainer+`"><div class="avi" style="`+avatar("receiver-username")+`">${localStorage.getItem('receiver-username')[0].toUpperCase()}</div><div class="bubble" style="`+chatbubbles+`">${data.message}</div></div>`
             document.documentElement.scrollTop = document.documentElement.scrollHeight;
 
         })  
@@ -177,13 +177,13 @@ imgdiv.innerHTML = "This is a paragraph."
       ></input>
 
 
-    <IconButton color="inherit" aria-label="open drawer" >
+    <IconButton color="inherit" aria-label="open drawer" style={{cursor:"pointer"}}>
         <label htmlFor="image"> 
-                <PhotoLibraryIcon />
+                <PhotoLibraryIcon style={{cursor:"pointer"}} />
         </label> 
     </IconButton>
 
-    {file && <img src={file} alt="Img" style={{width:"50px"}}></img>}
+    {file && <img src={file} alt="Img" style={{width:"50px", borderRadius:"10px"}}></img>}
 
           <IconButton color="inherit" aria-label="open drawer">
           <MicIcon />
