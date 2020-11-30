ba29e78ea72e6ac970e9f3ecfbb3ff71968aa786
ebisLab <ebaiden@techie.com>
2020-11-30 15:18:48 -0500

    logged in user profile redesigned

--- src/components/dashboard/usersSection/myProfile.js
+++ src/components/dashboard/usersSection/myProfile.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from "@material-ui/core/IconButton";
+import EditIcon from '@material-ui/icons/Edit';
+import GroupAddIcon from '@material-ui/icons/GroupAdd';
 
 function MyProfile(){
     const name = localStorage.getItem('myname')
 
     return(
         <div className="my-profile">
-            <div className="my-profile-icon"></div>
-            <h1>{name}</h1>
+            {/* <div className="my-profile-icon"></div>
+            <h1>{name}</h1> */}
+         <div style={{display:"inline-flex", width: "100%", padding:"10px"}}>
+                <div style={{display:"inline-flex", position: "relative", left: 0, width: "100%" }}>
+                 <Avatar>{name[0].toUpperCase()}</Avatar>
+            <h1>{name}</h1>   
+                </div>
+                <div style={{display:"inline-flex"}}>
+                <IconButton edge="end" color="inherit">
+            <EditIcon />
+          </IconButton>
+
+          <IconButton edge="end" color="inherit">
+            <GroupAddIcon />
+          </IconButton>
+                </div>
+        </div>
         </div>
     )
 }
